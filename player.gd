b79a272ab0f7cd12a476bc598fa185bd3b7ceba3
EshanDoes <eshan.shiraz@gmail.com>
2026-02-28 14:39:06 -0500

    Initial commit

---
+++ player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 700.0
+const JUMP_VELOCITY = -1000.0
+const START_POSITION = Vector2(436, 315)
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("restart"):
+		position=START_POSITION
+	
+	if position.y>3000.0:
+		position=START_POSITION
